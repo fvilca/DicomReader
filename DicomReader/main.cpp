49de80a9cc7c89bc86714338235d3a41b7f07fc7
F¡l <fvilca@gmail.com>
2017-06-24 20:31:57 -0500

    dicoim reader code, dont work

--- DicomReader/main.cpp
+++ DicomReader/main.cpp
@@ -1,7 +1,44 @@
 #include<iostream>
 using namespace std;
+#include <vtkSmartPointer.h>
+#include <vtkImageViewer2.h>
+#include <vtkDICOMImageReader.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+
 
 void main() {
-	cout << "Hola" << endl;
+	// Verify input arguments
+	/* if ( argc != 2 )
+	{
+	std::cout << "Usage: " << argv[0]
+	<< " Filename(.img)" << std::endl;
+	return EXIT_FAILURE;
+	}
+	*/
+	//std::string inputFilename = argv[1];
+	std::string inputFilename = "prostate.img";
+
+	// Read all the DICOM files in the specified directory.
+	vtkSmartPointer<vtkDICOMImageReader> reader = vtkSmartPointer<vtkDICOMImageReader>::New();
+	reader->SetFileName(inputFilename.c_str());
+	reader->Update();
+
+	// Visualize
+	vtkSmartPointer<vtkImageViewer2> imageViewer =
+		vtkSmartPointer<vtkImageViewer2>::New();
+	imageViewer->SetInputConnection(reader->GetOutputPort());
+
+	vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+		vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	imageViewer->SetupInteractor(renderWindowInteractor);
+	imageViewer->Render();
+	imageViewer->GetRenderer()->ResetCamera();
+	imageViewer->Render();
+
+	renderWindowInteractor->Start();
+
+	
 
 }
