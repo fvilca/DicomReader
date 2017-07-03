a2c4de8dfdb1cb040a46277dd62e514c9fc32f0c
F¡l <fvilca@gmail.com>
2017-07-03 17:32:25 -0500

    configs

--- DicomReader/main.cpp
+++ DicomReader/main.cpp
@@ -1,38 +1,11 @@
-#define vtkRenderingCore_AUTOINIT 2(vtkRenderingOpenGL2, vtkInteractionStyle)
-
-#include <vtkSmartPointer.h>
-#include <vtkImageViewer2.h>
-#include <vtkDICOMImageReader.h>
-#include <vtkRenderWindow.h>
-#include <vtkRenderWindowInteractor.h>
-#include <vtkRenderer.h>
-
 #include<iostream>
+//#include "DicomReader.h"
+
+#include "ReadUnstructuredGrid2.h"
 using namespace std;
 
-void main() {
-	
-	std::string inputFilename = "prostate.img";
-
-	// Read all the DICOM files in the specified directory.
-	vtkSmartPointer<vtkDICOMImageReader> reader = vtkSmartPointer<vtkDICOMImageReader>::New();
-	reader->SetFileName(inputFilename.c_str());
-	reader->Update();
-
-	// Visualize
-	vtkSmartPointer<vtkImageViewer2> imageViewer =
-		vtkSmartPointer<vtkImageViewer2>::New();
-	imageViewer->SetInputConnection(reader->GetOutputPort());
-
-	vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
-		vtkSmartPointer<vtkRenderWindowInteractor>::New();
-	imageViewer->SetupInteractor(renderWindowInteractor);
-	imageViewer->Render();
-	imageViewer->GetRenderer()->ResetCamera();
-	imageViewer->Render();
-
-	renderWindowInteractor->Start();
-
-	
+int main() {
+	//DicomReader();
+	execute();
 
 }
